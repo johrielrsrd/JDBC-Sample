83a84cdf1b2df3b96fdace3d547a85d6393cf00e
johrielrsrd <johrielrsrd@gmail.com>
2023-12-06 01:24:50 +0800

    initial commit

--- src/database/AppLauncher.java
+++ src/database/AppLauncher.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 import static database.DatabaseSample.*;
 
 public class AppLauncher {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
 //        dbWrite("johrielrsrd", "123456");
 //        dbWrite("johnrsrd", "1234");
         dbRead();
